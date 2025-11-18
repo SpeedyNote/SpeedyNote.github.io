864784873881fc0efa40d3e627c8e0874db1134f
alpha-liu-01 <116528196+alpha-liu-01@users.noreply.github.com>
2025-11-18 18:25:07 -0500

    Update Windows and Debian file versions

--- wget-sync.sh
+++ wget-sync.sh
@@ -3,12 +3,12 @@
 # 配置克隆文件名
 
 BASE_URL=https://speedynote.org
-WINDOWS=SpeedyNoteInstaller_Beta_0.10.7_amd64.exe
+WINDOWS=SpeedyNoteInstaller_Beta_0.11.1_amd64.exe
 WINDOWS_I386=SpeedyNoteInstaller_Beta_0.10.3_Qt5_i686.exe
 WINDOWS_ARM=SpeedyNoteInstaller_Beta_0.10.5-2_arm64.exe
 MACOS=SpeedyNote_v0.10.5_macOS.dmg
-DEBIAN=speedynote_0.10.7-1_amd64.deb
-DEBIAN_ARM=speedynote_0.10.7-1_arm64.deb
+DEBIAN=speedynote_0.11.1-1_amd64.deb
+DEBIAN_ARM=speedynote_0.11.1-1_arm64.deb
 FEDORA=speedynote-0.10.5-1.fc42.x86_64.rpm
 ALPINE_ARM=speedynote-0.10.5-r1.apk
 
